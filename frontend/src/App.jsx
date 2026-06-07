7a7cd4c3d9931afa97ddac1a41fa8789115e5ac1
DHRUV JAIN <dj4018386@gmail.com>
2026-06-07 21:19:16 +0530

    url changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import Pricing from './page/Pricing'
 import InterviewReport from './page/InterviewReport'
 
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://ai-mock-interview-96bw.onrender.com"
 
 const App = () => {
   const dispatch = useDispatch()
